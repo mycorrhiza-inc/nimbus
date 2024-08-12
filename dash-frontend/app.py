bc78eff86e9ee5644bf012d30431d569f5beeda2
Bradley Venner <bvenner@proton.me>
2024-08-11 20:51:15 -0600

    first buildgen figure

--- dash-frontend/app.py
+++ dash-frontend/app.py
@@ -1,7 +1,7 @@
 # Import packages
 from dash import Dash, html, dash_table, dcc, callback, Output, Input
 import pandas as pd
-import plotly.graph_objects as go
+import plotly.express as px
 from pathlib import Path
 
 # Incorporate data
@@ -13,30 +13,33 @@ file_geninfo = dir_input / 'gen_info.csv'
 buildgen = pd.read_csv(file_buildgen)
 buildgen.rename(columns = {'GEN_BLD_YRS_1': 'GENERATION_PROJECT', 'GEN_BLD_YRS_2': 'INVESTMENT_PERIOD'},inplace=True)
 geninfo = pd.read_csv(file_geninfo)
-buildgen.join(geninfo, on='GENERATION_PROJECT', how = 'left')
+gen_tech=geninfo[['GENERATION_PROJECT', 'gen_tech']]
+dumb=buildgen.merge(gen_tech, on='GENERATION_PROJECT', how = 'left')
+
 # Generate figure
+fig = px.bar(dumb, y='BuildGen',x='INVESTMENT_PERIOD',color="gen_tech")
 
-# Initialize the app
-app = Dash()
+def start_dash():
+    app = Dash()
 
-# App layout
-app.layout = [
-    html.Div(children='My First App with Data, Graph, and Controls'),
-    html.Hr(),
-    dcc.RadioItems(options=['pop', 'lifeExp', 'gdpPercap'], value='lifeExp', id='controls-and-radio-item'),
-    dash_table.DataTable(data=df.to_dict('records'), page_size=6),
-    dcc.Graph(figure={}, id='controls-and-graph')
-]
+    # App layout
+    app.layout = [
+        html.Div(children='Build Capacity'),
+        html.Hr(),
+        dcc.Graph(figure=fig, id='graph')
+    ]
 
-# Add controls to build the interaction
-@callback(
-    Output(component_id='controls-and-graph', component_property='figure'),
-    Input(component_id='controls-and-radio-item', component_property='value')
-)
-def update_graph(col_chosen):
-    fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg')
-    return fig
 
-# Run the app
-if __name__ == '__main__':
-    app.run_server(debug=True, host='0.0.0.0', port=9000)
+    # # Add controls to build the interaction
+    # @callback(
+    #     Output(component_id='controls-and-graph', component_property='figure'),
+    #     Input(component_id='controls-and-radio-item', component_property='value')
+    # )
+    # def update_graph(col_chosen):
+    #     fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg')
+    #     return fig
+    
+    app.run_server(debug=True, host='0.0.0.0', port=9001)
+
+if __name__=="__main__":
+    start_dash()
