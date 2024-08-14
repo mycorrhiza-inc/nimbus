dc923bef81504994f87e9f77c2f6143d93f529c7
Bradley Venner <bvenner@proton.me>
2024-08-14 14:02:45 -0600

    changes to actually get a model running

--- dash-frontend/app.py
+++ dash-frontend/app.py
@@ -3,32 +3,35 @@ from dash import Dash, html, dash_table, dcc, callback, Output, Input
 import pandas as pd
 import plotly.express as px
 from pathlib import Path
+import os
 
-# Incorporate data
-dir_scenario=Path('/home/brad/Documents/energy-democracy/gridmodel/data/switch/scc7a_60_fuel/')
-dir_output  = dir_scenario / 'outputs'
-dir_input = dir_scenario / 'inputs'
-file_buildgen = dir_output / 'BuildGen.csv'
-file_geninfo = dir_input / 'gen_info.csv'
-buildgen = pd.read_csv(file_buildgen)
-buildgen.rename(columns = {'GEN_BLD_YRS_1': 'GENERATION_PROJECT', 'GEN_BLD_YRS_2': 'INVESTMENT_PERIOD'},inplace=True)
-geninfo = pd.read_csv(file_geninfo)
-gen_tech=geninfo[['GENERATION_PROJECT', 'gen_tech']]
-# dumb=buildgen.merge(gen_tech, on='GENERATION_PROJECT', how = 'left')
+DEFAULT_SCENARIO_FOLDER = os.getenv("DEFAULT_SCENARIO_FOLDER","/home/brad/Documents/energy-democracy/gridmodel/data/switch/scc7a_60_fuel")
 
-# Generate figure
-# fig = px.bar(dumb, y='BuildGen',x='INVESTMENT_PERIOD',color="gen_tech")
+def build_figure(dir_scenario : Path):
+    dir_output  = dir_scenario / 'outputs'
+    dir_input = dir_scenario / 'inputs'
+    file_buildgen = dir_output / 'BuildGen.csv'
+    file_geninfo = dir_input / 'gen_info.csv'
+    buildgen = pd.read_csv(file_buildgen, dtype={})
+    buildgen.columns = ['GENERATION_PROJECT', 'INVESTMENT_PERIOD','BuildGen']
+    buildgen['GENERATION_PROJECT']=buildgen['GENERATION_PROJECT'].str.strip()
+    geninfo = pd.read_csv(file_geninfo)
+    gen_tech=geninfo[['GENERATION_PROJECT', 'gen_tech']]
+    gen_tech['GENERATION_PROJECT']=gen_tech['GENERATION_PROJECT'].str.strip()
+    dumb=buildgen.merge(gen_tech, on='GENERATION_PROJECT', how = 'left')
+    # Generate figure
+    fig = px.bar(dumb, y='BuildGen',x='INVESTMENT_PERIOD',color="gen_tech")
+    return fig
 
 def start_dash():
     app = Dash()
-
+    fig=build_figure(Path(DEFAULT_SCENARIO_FOLDER))
     # App layout
     app.layout = [
         html.Div(children='Build Capacity'),
         html.Hr(),
         dcc.Graph(figure=fig, id='graph')
     ]
-
 
     # # Add controls to build the interaction
     # @callback(
